44c272e0deef511af2219876b207812625d60d3f
Laurent LAMASSE <laurent.lamasse@gmail.com>
2017-08-01 09:29:15 +0200

    Ajout de la javadoc pour la couche DAO.

--- proxibanque-dao/src/main/java/com/gtm/proxibanque/dao/IGerantDao.java
+++ proxibanque-dao/src/main/java/com/gtm/proxibanque/dao/IGerantDao.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Repository;
 
 import com.gtm.proxibanque.domaine.Gerant;
 
+/**
+ * Interface fournissant les methodes de la couche DAO pour les objets de type Gerant.
+ * Cette interface herite de l'interface IGenericDao<Gerant>
+ */
 @Repository("gerantDao")
 public interface IGerantDao extends IGenericDao<Gerant>{
 
