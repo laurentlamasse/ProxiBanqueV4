4160a84e34ca6c45e10f88d3748f467e3bdac803
Clement Roux <roux-clement@laposte.net>
2017-08-02 12:01:47 +0200

    Modif front Ajout de l'AOP

---
+++ proxibanque-service/src/main/java/com/gtm/proxibanque/logger/Logger.java
@@ -0,0 +1,52 @@
+package com.gtm.proxibanque.logger;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+
+import com.gtm.proxibanque.domaine.Virement;
+
+import org.aspectj.lang.JoinPoint.StaticPart;
+
+public class Logger {
+
+	private final static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(Logger.class);
+	
+	public void logMethodEntry(JoinPoint joinPoint) {
+		Object[] args = joinPoint.getArgs();
+
+		// Nom de la m�thode intercept�e
+		String name = joinPoint.getSignature().toShortString();
+		StringBuffer sb = new StringBuffer(name + " appellé avec les paramètres suivants : [");
+
+		// Liste des valeurs des arguments re�us par la m�thode
+		for (int i = 0; i < args.length; i++) {
+			Object o = args[i];
+			if(Virement.class.isInstance(o)) {
+				Virement virement = (Virement) o;
+				sb.append("compte débité n° "+virement.getCompteDebite().getNumeroCompte()+", compte crédité n° "+virement.getCompteCredite().getNumeroCompte()+", montant : "+virement.getMontant()+" €");
+			}
+			//sb.append("'" + o + "'");
+			//sb.append((i == args.length - 1) ? "" : ", ");
+		}
+		sb.append("]");
+		//System.out.println(sb);
+		log.warn(sb);
+		
+	}
+	
+	public void logMethodExitSucces(StaticPart staticPart, Object resultat) {
+		// Information sur le point de jonction
+		String name = staticPart.getSignature().toShortString();
+		//System.out.println(name + " [le virement a été exécuté avec succès]");
+		log.warn(name + " [le virement a été exécuté avec succès]");
+	}
+
+//	public Object doBasicProfiling(ProceedingJoinPoint pjp) throws Throwable {
+//		String name = pjp.getSignature().toShortString();
+//		System.out.println("AVANT " + name);
+//		Object retVal = pjp.proceed();
+//		System.out.println("APRES");
+//		return retVal;
+//	}
+//	
+}
